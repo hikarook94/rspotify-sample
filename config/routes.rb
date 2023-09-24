2d78bf256b13c3b37085f19e39555a892480e475
hikarook94 <walrus4l145@icloud.com>
2023-09-24 16:04:37 +0900

    bin/rails g controller search index new

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'search/index'
+  get 'search/new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
