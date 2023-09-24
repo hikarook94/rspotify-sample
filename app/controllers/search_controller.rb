d9b46ac57055a5eff38638ad896d12599f1dc1ae
hikarook94 <walrus4l145@icloud.com>
2023-09-24 16:07:21 +0900

    表示・検索機能追加

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,13 @@
 class SearchController < ApplicationController
-  def index
+  def new
+    # 検索フォームの表示
   end
 
-  def new
+  def index
+    if params[:query].present?
+      @tracks = RSpotify::Track.search(params[:query])
+    else
+      redirect_to new_search_path, alert: "Please enter a search query."
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'search/index'
-  get 'search/new'
+  resources :search, only: [:new, :index]
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
