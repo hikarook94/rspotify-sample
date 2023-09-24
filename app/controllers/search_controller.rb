2c15862121c4bac3ec1b20bbf4b4ff027a2cae54
hikarook94 <walrus4l145@icloud.com>
2023-09-24 16:16:39 +0900

    rubocop -A

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,10 +1,6 @@
 # frozen_string_literal: true
 
 class SearchController < ApplicationController
-  def new
-    # 検索フォームの表示
-  end
-
   def index
     if params[:query].present?
       @tracks = RSpotify::Track.search(params[:query])
@@ -12,4 +8,8 @@ class SearchController < ApplicationController
       redirect_to new_search_path, alert: 'Please enter a search query.'
     end
   end
+  def new
+    # 検索フォームの表示
+  end
+
 end
